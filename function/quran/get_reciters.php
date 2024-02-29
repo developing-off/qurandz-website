c21436b3bc91d6f116b7515bf0c59532ea4f0df1
Younes Sarni <ysarni42@gmail.com>
2024-02-29 22:24:18 +0100

    Refactor get_reciters.php to use updated API endpoint and add new reciters

--- function/quran/get_reciters.php
+++ function/quran/get_reciters.php
@@ -1,43 +1,96 @@
 <?php
-$url = "https://api.quran.com/api/v4/resources/chapter_reciters";
+$url = "https://api.quran.com/api/v4/resources/recitations";
 $json = file_get_contents($url);
 $reciters_data = json_decode($json, TRUE);
+foreach ($reciters_data['recitations'] as $reciter) {
+    $reciter_name = $reciter['reciter_name'];
+}
 
-$reciters = $reciters_data['reciters'];
 function trad_reciters($reciter_name)
 {
     if ($reciter_name === 'Khalifah Taniji') {
         echo 'خليفة الطنيجي';
     }
-    if ($reciter_name === 'Abdur-Rahman as-Sudais') {
+    elseif ($reciter_name === 'Abdur-Rahman as-Sudais') {
         echo 'عبد الرحمن السديس';
     }
-    if ($reciter_name === 'Sa`ud ash-Shuraym') {
+    elseif ($reciter_name === 'Sa`ud ash-Shuraym') {
         echo 'سعود الشريم';
     }
-    if ($reciter_name === 'Mahmoud Khaleel Al-Husary') {
+    elseif ($reciter_name === 'Mahmoud Khaleel Al-Husary') {
         echo 'محمود خليل الحصري';
     }
-    if ($reciter_name === 'Abdul Basit Abdul Samad') {
+    elseif ($reciter_name === 'Abdul Basit Abdul Samad') {
         echo 'عبد الباسط عبد الصمد';
     }
-    if ($reciter_name === 'Mishari Rashid al-`Afasy') {
+    elseif ($reciter_name === 'Mishari Rashid al-`Afasy') {
         echo 'مشاري راشد العفاسي';
     }
-    if ($reciter_name === 'Abu Bakr al-Shatri') {
+    elseif ($reciter_name === 'Abu Bakr al-Shatri') {
         echo 'أبو بكر الشطري';
     }
-    if ($reciter_name === 'Muhammad Siddiq al-Minshawi') {
+    elseif ($reciter_name === 'Muhammad Siddiq al-Minshawi') {
         echo 'محمد صديق المنشاوي';
     }
-    if ($reciter_name === 'Hani ar-Rifai') {
+    elseif ($reciter_name === 'Hani ar-Rifai') {
         echo 'هاني الرفاعي';
     }
-    if ($reciter_name === 'Mahmoud Khalil Al-Husary') {
+    elseif ($reciter_name === 'Mahmoud Khalil Al-Husary') {
         echo 'محمود خليل الحصري';
     }
-    if ($reciter_name === 'Muhammad Siddiq al-Minshawi(with kids)') {
+    elseif ($reciter_name === 'Muhammad Siddiq al-Minshawi(with kids)') {
         echo 'محمد صديق المنشاوي';
+    }
+    elseif ($reciter_name === 'Abdullah Basfar') {
+        echo 'عبد الله بصفر';
+    }
+    elseif ($reciter_name === 'Abdullah Matrood') {
+        echo 'عبد الله مطرود';
+    }
+    elseif ($reciter_name === 'AbdulMuhsin al-Qasim') {
+        echo 'عبد المحسن القاسم';
+    }
+    elseif ($reciter_name === 'AbdulWadood Haneef') {
+        echo 'عبد الودود حنيف';
+    }
+    elseif ($reciter_name === 'Abu Bakr Ash-Shaatree') {
+        echo 'أبو بكر الشاطري';
+    }
+    elseif ($reciter_name === 'Ahmed ibn Ali al-Ajamy') {
+        echo 'أحمد بن علي العجمي';
+    }
+    elseif ($reciter_name === 'Ahmed Neana') {
+        echo 'أحمد نعينع';
+    }
+    elseif ($reciter_name === 'Alafasy') {
+        echo 'العفاسي';
+    }
+    elseif ($reciter_name === 'Ali al-Huthaify') {
+        echo 'علي الحذيفي';
+    }
+    elseif ($reciter_name === 'Ali Hajjaj Alsouasi') {
+        echo 'علي حجاج السويسي';
+    }
+    elseif ($reciter_name === 'Aziz Alili') {
+        echo 'عزيز علي';
+    }
+    elseif ($reciter_name === 'Fares Abbad') {
+        echo 'فارس عباد';
+    }
+    elseif ($reciter_name === 'Hani Ar Rifai') {
+        echo 'هاني الرفاعي';
+    }
+    elseif ($reciter_name === 'Ibrahim Al Akhdar') {
+        echo 'إبراهيم الأخضر';
+    }
+    elseif ($reciter_name === 'Khalid Abdulkafi') {
+        echo 'خالد عبد الكافي';
+    }
+    elseif ($reciter_name === 'Maher Al Mueaqly') {
+        echo 'ماهر المعيقلي';
+    }else{
+        echo $reciter_name;
+    
     }
 }
 
@@ -46,22 +99,25 @@ function trad_reciters($reciter_name)
     <div class="container">
         <div class="row mt-50">
             <?php
-            if($reciters){
+            if($reciter_name){
 
            
             $i = 0;
-            foreach ($reciters as $reciter) {
+            foreach ($reciters_data['recitations'] as $reciter) {
                 ?>
                 <div class="col-lg-3 col-md-6 col-sm-6 ">
                     <div class="card-offer hover-up">
                         <div class="card-info">
                             <h4 class="color-brand-2 mb-5" style="font-size: 18px;">القارئ:
-                                <?php trad_reciters($reciter['name']); ?>
+                                <?php trad_reciters($reciter['reciter_name']); ?>
                             </h4>
                             <p class="font-md color-white mb-5">قراءة:
-                                <?php if ($reciter['qirat']['name'] === "Hafs") {
+                                <?php if ($reciter['style'] === "Hafs") {
                                     echo 'حفص';
-                                } ?>
+                                }else{
+                                    echo $reciter['style'];
+                                } 
+                                ?>
                             </p>
                             <div class="box-button-offer">
                                 <?php
